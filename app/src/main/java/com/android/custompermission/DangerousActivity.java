14b6f239a700a104622701e98c7186a98968819c
ChenTao <935612713@qq.com>
2018-03-12 00:06:01 +0800

    [feature] 自定义权限出现提交

---
+++ app/src/main/java/com/android/custompermission/DangerousActivity.java
@@ -0,0 +1,13 @@
+package com.android.custompermission;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DangerousActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dangerous);
+    }
+}
---
+++ app/src/main/java/com/android/custompermission/NormalActivity.java
@@ -0,0 +1,13 @@
+package com.android.custompermission;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NormalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal);
+    }
+}
